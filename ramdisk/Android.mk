d9f814f266dd7828c0a181bd8e8bd4e15e75300f
Christopher N. Hesse <raymanfx@gmail.com>
2016-05-24 17:03:26 +0200

    gts210ltexx: Fix modem/baseband init

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -3,10 +3,10 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter gts210ltexx, $(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.modem.rc
+LOCAL_MODULE		:= init.baseband.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= BOOT
-LOCAL_SRC_FILES		:= init.modem.rc
+LOCAL_SRC_FILES		:= init.baseband.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
