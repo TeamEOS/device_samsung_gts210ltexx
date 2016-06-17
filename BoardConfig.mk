c7a485832b0e5e28b1ecc892472aa89334b2d83b
Jan Huijsmans <huysmans@koffie.nu>
2016-06-17 10:30:11 +0200

    RIL: Moved from gts2-common tree, lte only
    Change-Id: I94d4c30710d14189ad7d482d1a5190eefb3d4699

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,12 @@ TARGET_KERNEL_CONFIG := cm_exynos5433-gts210lte_defconfig
 
 # RIL
 BOARD_MODEM_TYPE := ss333
+BOARD_PROVIDES_LIBRIL := true
+BOARD_MODEM_NEEDS_VIDEO_CALL_FIELD := true
+
+# RIL.java overwrite
+BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril
+
 
 # Recovery
 TARGET_OTA_ASSERT_DEVICE := gts210lte,gts210ltexx
