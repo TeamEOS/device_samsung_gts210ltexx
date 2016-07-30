dd4492fc97600b03639e3892db77e2edeb990db8
Christopher N. Hesse <raymanfx@gmail.com>
2016-07-30 17:15:29 +0200

    gts210ltexx: Sanitize makefile comments
    Change-Id: If50db7381717d5ce6bddf948ae04b0c6db32c7b8

--- device.mk
+++ device.mk
@@ -1,30 +1,18 @@
 LOCAL_PATH := device/samsung/gts210ltexx
 
-###########################################################
-### FLAT DEVICE TREE
-###########################################################
-
+# Flat device tree for boot image
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/dtb.img:dt.img
 
-###########################################################
-### RAMDISK
-###########################################################
-
+# Ramdisk
 PRODUCT_PACKAGES += \
     init.baseband.rc
 
-###########################################################
-### AUDIO
-###########################################################
-
+# Tinyalsa
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
-###########################################################
-### BLUETOOTH
-###########################################################
-
+# Bluetooth
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
 
